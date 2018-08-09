2ce8ce90dc959614fcaee462b04d99b8db6caf72
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-09 12:34:27 +0200

    La til funksjoner for uthenting av dei ulike skjemaene

--- src/main/kotlin/no/nav/paop/PaopApplication.kt
+++ src/main/kotlin/no/nav/paop/PaopApplication.kt
@@ -7,10 +7,14 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.prometheus.client.hotspot.DefaultExports
 import kotlinx.coroutines.experimental.runBlocking
 import no.nav.model.dataBatch.DataBatch
+import no.nav.model.navOppfPlan.OppfolgingsplanMetadata
+import no.nav.model.oppfolgingsplan2014.Oppfoelgingsplan2M
+import no.nav.model.oppfolgingsplan2016.Oppfoelgingsplan4UtfyllendeInfoM
 import org.slf4j.LoggerFactory
 import java.io.StringReader
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Unmarshaller
+import javax.xml.transform.stream.StreamSource
 
 val objectMapper: ObjectMapper = ObjectMapper()
         .registerModule(JavaTimeModule())
@@ -25,7 +29,6 @@ fun main(args: Array<String>) = runBlocking {
     DefaultExports.initialize()
     val fasitProperties = FasitProperties()
     createHttpServer(applicationVersion = fasitProperties.appVersion)
-    log.info("Hello there")
 
     // TODO read for kafak topic
     // aapen-altinn-oppfolgingsplan-Mottatt
@@ -33,8 +36,35 @@ fun main(args: Array<String>) = runBlocking {
     // val dataBatch = extractDataBatchFromString(inputMessageText)
 }
 
-fun extractDataBatchFromString(dataBatchString: String): DataBatch {
+fun extractDataBatch(dataBatchString: String): DataBatch {
     val dataBatchJaxBContext: JAXBContext = JAXBContext.newInstance(DataBatch::class.java)
     val dataBatchUnmarshaller: Unmarshaller = dataBatchJaxBContext.createUnmarshaller()
     return dataBatchUnmarshaller.unmarshal(StringReader(dataBatchString)) as DataBatch
+}
+
+fun extractOppfolginsplan2016(formdataString: String): Oppfoelgingsplan4UtfyllendeInfoM {
+    val skjemainnholdJaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan4UtfyllendeInfoM::class.java)
+    val skjemainnholdUnmarshaller: Unmarshaller = skjemainnholdJaxBContext.createUnmarshaller()
+    return skjemainnholdUnmarshaller.unmarshal(
+            StreamSource(StringReader(formdataString)), Oppfoelgingsplan4UtfyllendeInfoM::class.java).value
+}
+
+fun extractOppfolginsplan2014(formdataString: String): Oppfoelgingsplan2M {
+    val oppfoelgingsplan2MJaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan2M::class.java)
+    val oppfoelgingsplan2MUnmarshaller: Unmarshaller = oppfoelgingsplan2MJaxBContext.createUnmarshaller()
+    return oppfoelgingsplan2MUnmarshaller.unmarshal(
+            StreamSource(StringReader(formdataString)), Oppfoelgingsplan2M::class.java).value
+}
+
+fun extractOppfolginsplan2012(formdataString: String): no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M {
+    val oppfoelgingsplan2MJaxBContext: JAXBContext = JAXBContext.newInstance(no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M::class.java)
+    val oppfoelgingsplan2MUnmarshaller: Unmarshaller = oppfoelgingsplan2MJaxBContext.createUnmarshaller()
+    return oppfoelgingsplan2MUnmarshaller.unmarshal(
+            StreamSource(StringReader(formdataString)), no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M::class.java).value
+}
+
+fun extractNavOppfPlan(formdataString: String): OppfolgingsplanMetadata {
+    val oppfolgingsplanMetadataJaxBContext: JAXBContext = JAXBContext.newInstance(OppfolgingsplanMetadata::class.java)
+    val oppfolgingsplanMetadataUnmarshaller: Unmarshaller = oppfolgingsplanMetadataJaxBContext.createUnmarshaller()
+    return oppfolgingsplanMetadataUnmarshaller.unmarshal(StringReader(formdataString)) as OppfolgingsplanMetadata
 }
--- src/test/kotlin/no/nav/paop/PaopApplicationTest.kt
+++ src/test/kotlin/no/nav/paop/PaopApplicationTest.kt
@@ -1,42 +1,45 @@
 package no.nav.paop
 
 import org.amshove.kluent.shouldEqual
+import org.slf4j.LoggerFactory
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.time.LocalDateTime
 import java.time.Month
 
+private val log = LoggerFactory.getLogger("nav.paop-applicationTest")
+
 object PaopApplicationTest : Spek({
 
-    describe("tests the functions in PaopApplication") {
+    describe("tests the functions extractDataBatch") {
 
         it("Should set databatch.schemaVersion") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.schemaVersion shouldEqual 1.0.toBigDecimal()
         }
         it("Should set databatch.attachments") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.attachments shouldEqual null
         }
         it("Should set databatch.batchReference") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.batchReference shouldEqual 85385
         }
         it("Should set databatch.dataUnits.dataUnit[0].reportee") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().reportee shouldEqual "923475230"
         }
         it("Should set databatch.dataUnits.dataUnit[0].archiveReference") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().archiveReference shouldEqual "AR387469726"
         }
         it("Should set databatch.dataUnits.dataUnit[0].archiveTimeStamp") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             val expectedDate = LocalDateTime.of(2017, Month.MARCH, 14, 12, 52)
             dataBatch.dataUnits.dataUnit.first().archiveTimeStamp.year shouldEqual expectedDate.year
@@ -46,34 +49,81 @@ object PaopApplicationTest : Spek({
             dataBatch.dataUnits.dataUnit.first().archiveTimeStamp.minute shouldEqual expectedDate.minute
         }
         it("Should set databatch.dataUnits.dataUnit[0].formTask.serviceCode") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().formTask.serviceCode shouldEqual "2913"
         }
         it("Should set databatch.dataUnits.dataUnit[0]formTask.serviceEditionCode") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().formTask.serviceEditionCode shouldEqual "4"
         }
         it("Should set databatch.dataUnits.dataUnit[0].formTask.form[0].dataFormatId") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().formTask.form.first().dataFormatId shouldEqual "3521"
         }
         it("Should set databatch.dataUnits.dataUnit[0].formTask.form[0].dataFormatVersion") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().formTask.form.first().dataFormatVersion shouldEqual "37865"
         }
         it("Should set databatch.dataUnits.dataUnit[0].formTask.form[0].reference") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
             dataBatch.dataUnits.dataUnit.first().formTask.form.first().reference shouldEqual "77423963"
         }
-        it("Should set databatch.dataUnits.dataUnit[0].formTask.form[0].parentReference") {
-            val dataBatch = extractDataBatchFromString(getFileAsString(
+    }
+
+    describe("tests the functions extractOppfolginsplan2016") {
+        it("Should set dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData to OP2016") {
+            val dataBatch = extractDataBatch(getFileAsString(
                     "src/test/resources/oppfolging_2913_04.xml"))
-            dataBatch.dataUnits.dataUnit.first().formTask.form.first().parentReference shouldEqual "0"
+            dataBatch.dataUnits.dataUnit.first().formTask.form.first().reference shouldEqual "77423963"
+
+            val op2016 = extractOppfolginsplan2016(
+                    dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData)
+
+            op2016.skjemainnhold.arbeidsgiver.value.annenKontaktpersonEtternavn.value shouldEqual "Etternavn"
+        }
+    }
+
+    describe("tests the functions extractOppfolginsplan2014") {
+        it("Should set dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData to OP2014") {
+            val dataBatch = extractDataBatch(getFileAsString(
+                    "src/test/resources/oppfolging_2913_03.xml"))
+            dataBatch.dataUnits.dataUnit.first().formTask.form.first().reference shouldEqual "77423963"
+
+            val op2014 = extractOppfolginsplan2014(
+                    dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData)
+
+            op2014.skjemainnhold.arbeidsgiver.value.annenKontaktpersonEtternavn.value shouldEqual "Navnesen"
+        }
+    }
+
+    describe("tests the functions extractOppfolginsplan2012") {
+        it("Should set dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData to OP2012") {
+            val dataBatch = extractDataBatch(getFileAsString(
+                    "src/test/resources/oppfolging_2913_02.xml"))
+            dataBatch.dataUnits.dataUnit.first().formTask.form.first().reference shouldEqual "77426094"
+
+            val op2012 = extractOppfolginsplan2012(
+                    dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData)
+
+            op2012.skjemainnhold.arbeidsgiver.value.annenKontaktpersonEtternavn.value shouldEqual "Navnesen"
+        }
+    }
+
+    describe("tests the functions extractNavOppfPlan") {
+        it("Should set dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData to NavOppfPlan") {
+            val dataBatch = extractDataBatch(getFileAsString(
+                    "src/test/resources/oppfolging_navoppfplan_rapportering_sykemeldte.xml"))
+            dataBatch.dataUnits.dataUnit.first().formTask.form.first().reference shouldEqual "rapportering-sykmeldte"
+
+            val navOppfPlan = extractNavOppfPlan(
+                    dataBatch.dataUnits.dataUnit.first().formTask.form.first().formData)
+
+            navOppfPlan.fodselsNr shouldEqual "01010112345"
         }
     }
 })
