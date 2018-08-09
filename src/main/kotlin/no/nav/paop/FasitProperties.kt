4c021a301cc0a68c72206830991e2b85abfa5d6a
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-09 08:28:27 +0200

    Fikset på srvpaop bruker og ein pakke

--- src/main/kotlin/no/nav/paop/FasitProperties.kt
+++ src/main/kotlin/no/nav/paop/FasitProperties.kt
@@ -1,8 +1,8 @@
 package no.nav.paop
 
 data class FasitProperties(
-    val srvPaopUsername: String = getEnvVar("SRVPALE_USERNAME"),
-    val srvPaopPassword: String = getEnvVar("SRVPALE_PASSWORD"),
+    val srvPaopUsername: String = getEnvVar("SRVPAOP_USERNAME"),
+    val srvPaopPassword: String = getEnvVar("SRVPAOP_PASSWORD"),
     val appName: String = getEnvVar("APP_NAME"),
     val appVersion: String = getEnvVar("APP_VERSION")
 )
