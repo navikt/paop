08a5de9f2c9efc714346985bf3a9e320428573c3
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-14 09:25:04 +0200

    Fjernet aareg

--- src/main/kotlin/no/nav/paop/FasitProperties.kt
+++ src/main/kotlin/no/nav/paop/FasitProperties.kt
@@ -3,9 +3,6 @@ package no.nav.paop
 data class FasitProperties(
     val srvPaopUsername: String = getEnvVar("SRVPAOP_USERNAME"),
     val srvPaopPassword: String = getEnvVar("SRVPAOP_PASSWORD"),
-    val aaregHentOrganisasjonEndpointURL: String = getEnvVar("AAREG_HENT_ORGANISASJON_ENDPOINTURL"),
-    val aaregWSUsername: String = getEnvVar("AAREGPOLICYUSER_USERNAME"),
-    val aaregWSPassword: String = getEnvVar("AAREGPOLICYUSER_PASSWORD"),
     val journalbehandlingEndpointURL: String = getEnvVar("JOARK_JOURNALBEHANDLING_WS_ENDPOINT_URL"),
     val securityTokenServiceUrl: String = getEnvVar("SECURITYTOKENSERVICE_URL"),
     val appName: String = getEnvVar("APP_NAME"),
