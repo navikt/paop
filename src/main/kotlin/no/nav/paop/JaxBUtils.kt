0c9341914179afc836367e5f5007eec399033ccf
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-15 08:57:43 +0200

    Minor cleanups

---
+++ src/main/kotlin/no/nav/paop/JaxBUtils.kt
@@ -0,0 +1,47 @@
+package no.nav.paop
+
+import no.nav.model.dataBatch.DataBatch
+import no.nav.model.navOppfPlan.OppfolgingsplanMetadata
+import no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M as Oppfoelgingsplan2M2012
+import no.nav.model.oppfolgingsplan2014.Oppfoelgingsplan2M as Oppfoelgingsplan2M2014
+import no.nav.model.oppfolgingsplan2016.Oppfoelgingsplan4UtfyllendeInfoM
+import java.io.StringReader
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.Unmarshaller
+import javax.xml.datatype.DatatypeFactory
+import javax.xml.transform.stream.StreamSource
+
+val newInstance: DatatypeFactory = DatatypeFactory.newInstance()
+
+val dataBatchJaxBContext: JAXBContext = JAXBContext.newInstance(DataBatch::class.java)
+val dataBatchUnmarshaller: Unmarshaller = dataBatchJaxBContext.createUnmarshaller()
+
+val skjemainnholdJaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan4UtfyllendeInfoM::class.java)
+val skjemainnholdUnmarshaller: Unmarshaller = skjemainnholdJaxBContext.createUnmarshaller()
+
+val oppfoelgingsplan2014JaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan2M2014::class.java)
+val oppfoelgingsplan2014Unmarshaller: Unmarshaller = oppfoelgingsplan2014JaxBContext.createUnmarshaller()
+
+val oppfoelgingsplan2012JaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan2M2012::class.java)
+val oppfoelgingsplan2012Unmarshaller: Unmarshaller = oppfoelgingsplan2012JaxBContext.createUnmarshaller()
+
+val oppfolgingsplanMetadataJaxBContext: JAXBContext = JAXBContext.newInstance(OppfolgingsplanMetadata::class.java)
+val oppfolgingsplanMetadataUnmarshaller: Unmarshaller = oppfolgingsplanMetadataJaxBContext.createUnmarshaller()
+
+fun extractDataBatch(dataBatchString: String): DataBatch =
+        dataBatchUnmarshaller.unmarshal(StringReader(dataBatchString)) as DataBatch
+
+fun extractOppfolginsplan2016(formdataString: String): Oppfoelgingsplan4UtfyllendeInfoM =
+        skjemainnholdUnmarshaller.unmarshal(StreamSource(StringReader(formdataString)), Oppfoelgingsplan4UtfyllendeInfoM::class.java).value
+
+
+fun extractOppfolginsplan2014(formdataString: String): Oppfoelgingsplan2M2012 =
+        oppfoelgingsplan2014Unmarshaller.unmarshal(StreamSource(StringReader(formdataString)), Oppfoelgingsplan2M2012::class.java).value
+
+fun extractOppfolginsplan2012(formdataString: String): no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M {
+    return oppfoelgingsplan2012Unmarshaller.unmarshal(StreamSource(StringReader(formdataString)), no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M::class.java).value
+}
+
+fun extractNavOppfPlan(formdataString: String): OppfolgingsplanMetadata {
+    return oppfolgingsplanMetadataUnmarshaller.unmarshal(StringReader(formdataString)) as OppfolgingsplanMetadata
+}
--- src/main/kotlin/no/nav/paop/PaopApplication.kt
+++ src/main/kotlin/no/nav/paop/PaopApplication.kt
@@ -1,19 +1,11 @@
 package no.nav.paop
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.ibm.mq.jms.MQConnectionFactory
 import com.ibm.msg.client.wmq.WMQConstants
 import com.ibm.msg.client.wmq.compat.base.internal.MQC
 import io.prometheus.client.hotspot.DefaultExports
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
-import no.nav.model.dataBatch.DataBatch
-import no.nav.model.navOppfPlan.OppfolgingsplanMetadata
-import no.nav.model.oppfolgingsplan2014.Oppfoelgingsplan2M
-import no.nav.model.oppfolgingsplan2016.Oppfoelgingsplan4UtfyllendeInfoM
 import no.nav.paop.client.PdfClient
 import no.nav.paop.client.PdfType
 import no.nav.paop.client.SarClient
@@ -31,35 +23,22 @@ import org.apache.wss4j.common.ext.WSPasswordCallback
 import org.apache.wss4j.dom.WSConstants
 import org.apache.wss4j.dom.handler.WSHandlerConstants
 import org.slf4j.LoggerFactory
-import java.io.StringReader
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.jms.Connection
 import javax.jms.Queue
 import javax.security.auth.callback.CallbackHandler
-import javax.xml.bind.JAXBContext
-import javax.xml.bind.Unmarshaller
-import javax.xml.datatype.DatatypeFactory
-import javax.xml.transform.stream.StreamSource
-
-val objectMapper: ObjectMapper = ObjectMapper()
-        .registerModule(JavaTimeModule())
-        .registerKotlinModule()
-        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
 private val log = LoggerFactory.getLogger("nav.paop-application")
-val newInstance: DatatypeFactory = DatatypeFactory.newInstance()
-
-class PaopApplication
 
 fun main(args: Array<String>) = runBlocking {
     DefaultExports.initialize()
     val fasitProperties = FasitProperties()
     createHttpServer(applicationVersion = fasitProperties.appVersion)
 
-    // TODO read for kafak topic
-    // aapen-altinn-oppfolgingsplan-Mottatt
-    // all of the diffrent types of oppfolgingsplan comes throw here
+    // TODO read from kafka topic
+    // aapen-altinn-oppfolgingsplan-Mottatt, maybe change routing in altinnkanal-2 for 3 different topics?
+    // all of the different types of oppfolgingsplan comes throguh here
 
     connectionFactory(fasitProperties).createConnection(fasitProperties.mqUsername, fasitProperties.mqPassword).use {
         connection ->
@@ -119,8 +98,6 @@ fun listen(
     arenaQueue: Queue,
     connection: Connection
 ) = launch {
-
-    // Then ckeck the input if it is duplicate
     val dataBatch = extractDataBatch("asd")
     val serviceCode = dataBatch.dataUnits.dataUnit.first().formTask.serviceCode
     val serviceEditionCode = dataBatch.dataUnits.dataUnit.first().formTask.serviceEditionCode
@@ -185,39 +162,6 @@ fun listen(
         val extractOppfolginsplan = extractNavOppfPlan(formData)
         dataBatch.attachments.attachment.first().value // the pdf to store in joark
     }
-}
-
-fun extractDataBatch(dataBatchString: String): DataBatch {
-    val dataBatchJaxBContext: JAXBContext = JAXBContext.newInstance(DataBatch::class.java)
-    val dataBatchUnmarshaller: Unmarshaller = dataBatchJaxBContext.createUnmarshaller()
-    return dataBatchUnmarshaller.unmarshal(StringReader(dataBatchString)) as DataBatch
-}
-
-fun extractOppfolginsplan2016(formdataString: String): Oppfoelgingsplan4UtfyllendeInfoM {
-    val skjemainnholdJaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan4UtfyllendeInfoM::class.java)
-    val skjemainnholdUnmarshaller: Unmarshaller = skjemainnholdJaxBContext.createUnmarshaller()
-    return skjemainnholdUnmarshaller.unmarshal(
-            StreamSource(StringReader(formdataString)), Oppfoelgingsplan4UtfyllendeInfoM::class.java).value
-}
-
-fun extractOppfolginsplan2014(formdataString: String): Oppfoelgingsplan2M {
-    val oppfoelgingsplan2MJaxBContext: JAXBContext = JAXBContext.newInstance(Oppfoelgingsplan2M::class.java)
-    val oppfoelgingsplan2MUnmarshaller: Unmarshaller = oppfoelgingsplan2MJaxBContext.createUnmarshaller()
-    return oppfoelgingsplan2MUnmarshaller.unmarshal(
-            StreamSource(StringReader(formdataString)), Oppfoelgingsplan2M::class.java).value
-}
-
-fun extractOppfolginsplan2012(formdataString: String): no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M {
-    val oppfoelgingsplan2MJaxBContext: JAXBContext = JAXBContext.newInstance(no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M::class.java)
-    val oppfoelgingsplan2MUnmarshaller: Unmarshaller = oppfoelgingsplan2MJaxBContext.createUnmarshaller()
-    return oppfoelgingsplan2MUnmarshaller.unmarshal(
-            StreamSource(StringReader(formdataString)), no.nav.model.oppfolgingsplan2012.Oppfoelgingsplan2M::class.java).value
-}
-
-fun extractNavOppfPlan(formdataString: String): OppfolgingsplanMetadata {
-    val oppfolgingsplanMetadataJaxBContext: JAXBContext = JAXBContext.newInstance(OppfolgingsplanMetadata::class.java)
-    val oppfolgingsplanMetadataUnmarshaller: Unmarshaller = oppfolgingsplanMetadataJaxBContext.createUnmarshaller()
-    return oppfolgingsplanMetadataUnmarshaller.unmarshal(StringReader(formdataString)) as OppfolgingsplanMetadata
 }
 
 fun connectionFactory(fasitProperties: FasitProperties) = MQConnectionFactory().apply {
