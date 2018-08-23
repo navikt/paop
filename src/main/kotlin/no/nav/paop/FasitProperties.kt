a07eca87c71bdb8a481e1e010a94d64176fd7026
Joakim kartveit <joakim.kartveit@nav.no>
2018-08-23 14:22:10 +0200

    fikset formatering problemer

--- src/main/kotlin/no/nav/paop/FasitProperties.kt
+++ src/main/kotlin/no/nav/paop/FasitProperties.kt
@@ -21,7 +21,7 @@ data class FasitProperties(
     val kuhrSarApiURL: String = getEnvVar("KUHR_SAR_API_URL", "http://kuhr-sar-api"),
     val organisasjonv4EndpointURL: String = getEnvVar("VIRKSOMHET_ORGANISASJON_V4_ENDPOINTURL"),
     val kafkaBootstrapServersURL: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
-    val kafkaTopicOppfolginsplan: String = getEnvVar("AAPEN_PAOP_OPPFOLGINGSPLAN_TEST_MOTTATT_TOPIC","aapen-paop-oppfolgingsplan-test-motatt" )
+    val kafkaTopicOppfolginsplan: String = getEnvVar("AAPEN_PAOP_OPPFOLGINGSPLAN_TEST_MOTTATT_TOPIC", "aapen-paop-oppfolgingsplan-test-motatt")
 
 )
 
