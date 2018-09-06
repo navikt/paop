edc4f11ddc6275d03d0351368c5be352821e230f
Joakim kartveit <joakim.kartveit@nav.no>
2018-09-06 14:43:33 +0200

    La til oppretelse av objekter

--- src/main/kotlin/no/nav/paop/client/ArenaClient.kt
+++ src/main/kotlin/no/nav/paop/client/ArenaClient.kt
@@ -1,7 +1,10 @@
 package no.nav.paop.client
 
 import no.nav.model.arenaBrevTilArbeidsgiver.ArenaBrevTilArbeidsgiver
+import no.nav.model.arenaOppfolging.ArbeidsgiverType
 import no.nav.model.arenaOppfolging.ArenaOppfolgingPlan
+import no.nav.model.arenaOppfolging.DokumentInfoType
+import no.nav.model.arenaOppfolging.EiaDokumentInfoType
 import no.nav.model.dataBatch.DataBatch
 import no.nav.paop.PaopConstant
 import no.nav.paop.extractOppfolginsplan2012
@@ -11,27 +14,27 @@ import java.util.GregorianCalendar
 fun createArenaOppfolgingsplan(dataBatch: DataBatch, formData: String, edilogg: String):
         ArenaOppfolgingPlan = ArenaOppfolgingPlan().apply {
             val extractOppfolginsplan = extractOppfolginsplan2012(formData)
-            eiaDokumentInfo = eiaDokumentInfo.apply {
-                dokumentInfo = dokumentInfo.apply {
+            eiaDokumentInfo = EiaDokumentInfoType().apply {
+                dokumentInfo = DokumentInfoType().apply {
                     dokumentType = PaopConstant.dokumentType2913.string
                     dokumentreferanse = dataBatch.dataUnits.dataUnit.first().archiveReference
                     ediLoggId = edilogg
                     dokumentDato = newInstance.newXMLGregorianCalendar(GregorianCalendar())
                 }
                 behandlingInfo = null
-                avsender = avsender.apply {
-                    arbeidsgiver = arbeidsgiver.apply {
+                avsender = EiaDokumentInfoType.Avsender().apply {
+                    arbeidsgiver = ArbeidsgiverType().apply {
                         arbeidsgiverOrgNr = extractOppfolginsplan.skjemainnhold.arbeidsgiver.value.orgnr
                     }
                 }
-                avsenderSystem = avsenderSystem.apply {
+                avsenderSystem = EiaDokumentInfoType.AvsenderSystem().apply {
                     systemNavn = extractOppfolginsplan.skjemainnhold.avsenderSystem.value.systemNavn
                     systemVersjon = extractOppfolginsplan.skjemainnhold.avsenderSystem.value.systemVersjon
                 }
             }
             bedriftsNr = extractOppfolginsplan.skjemainnhold.arbeidsgiver.value.orgnr
             fodselsNr = extractOppfolginsplan.skjemainnhold.sykmeldtArbeidstaker.value.fnr
-            bistandNav = bistandNav.apply {
+            bistandNav = ArenaOppfolgingPlan.BistandNav().apply {
                 isBistandNavHjelpemid = extractOppfolginsplan.skjemainnhold.tiltak.value.tiltaksinformasjon.first()
                         .bistandHjelpemidler.value
                 isBistandNavVeil = extractOppfolginsplan.skjemainnhold.tiltak.value.tiltaksinformasjon.first()
@@ -46,15 +49,15 @@ fun createArenaOppfolgingsplan(dataBatch: DataBatch, formData: String, edilogg: 
 fun createArenaBrevTilArbeidsgiver(dataBatch: DataBatch, formData: String, edilogg: String):
         ArenaBrevTilArbeidsgiver = ArenaBrevTilArbeidsgiver().apply {
     val extractOppfolginsplan = extractOppfolginsplan2012(formData)
-    eiaDokumentInfo = eiaDokumentInfo.apply {
-        dokumentInfo = dokumentInfo.apply {
+    eiaDokumentInfo = no.nav.model.arenaBrevTilArbeidsgiver.EiaDokumentInfoType().apply {
+        dokumentInfo = no.nav.model.arenaBrevTilArbeidsgiver.DokumentInfoType().apply {
             dokumentType = "EIA.OFP_AG"
             dokumentTypeVersjon = "1.0"
             dokumentreferanse = dataBatch.dataUnits.dataUnit.first().archiveReference
             ediLoggId = edilogg
         }
-        avsender = avsender.apply {
-            arbeidsgiver = arbeidsgiver.apply {
+        avsender = no.nav.model.arenaBrevTilArbeidsgiver.EiaDokumentInfoType.Avsender().apply {
+            arbeidsgiver = no.nav.model.arenaBrevTilArbeidsgiver.ArbeidsgiverType().apply {
                 arbeidsgiverOrgNr = extractOppfolginsplan.skjemainnhold.arbeidsgiver.value.orgnr
             }
         }
