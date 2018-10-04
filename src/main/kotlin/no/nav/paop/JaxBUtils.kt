da48411c64a54a8d8396d701431d8ec40b5f8b8d
Joakim kartveit <joakim.kartveit@nav.no>
2018-10-04 08:36:38 +0200

    La til fellestype og fagtype i marsheller

--- src/main/kotlin/no/nav/paop/JaxBUtils.kt
+++ src/main/kotlin/no/nav/paop/JaxBUtils.kt
@@ -1,5 +1,7 @@
 package no.nav.paop
 
+import no.nav.model.arena.brev.FagType
+import no.nav.model.arena.brev.FellesType
 import no.nav.model.arena.brevdata.Brevdata
 import no.nav.model.arenaBrevTilArbeidsgiver.ArenaBrevTilArbeidsgiver
 import no.nav.model.arenaOppfolging.ArenaOppfolgingPlan
@@ -38,7 +40,7 @@ val arenaMarshaller: Marshaller = arenaEiaInfoJaxBContext.createMarshaller()
 val arenabrevnfoJaxBContext: JAXBContext = JAXBContext.newInstance(ArenaBrevTilArbeidsgiver::class.java)
 val arenabrevMarshaller: Marshaller = arenabrevnfoJaxBContext.createMarshaller()
 
-val arenabrevdataJaxBContext: JAXBContext = JAXBContext.newInstance(Brevdata::class.java)
+val arenabrevdataJaxBContext: JAXBContext = JAXBContext.newInstance(Brevdata::class.java, FellesType::class.java, FagType::class.java)
 val arenabrevdataMarshaller: Marshaller = arenabrevdataJaxBContext.createMarshaller()
 
 fun extractDataBatch(dataBatchString: String): DataBatch =
