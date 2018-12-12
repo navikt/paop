ffefcd59c948fcc53d9b63c329e152e91e141902
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-12 08:23:22 +0100

    Staret med å lese fra riktig kafka topic

--- src/main/kotlin/no/nav/paop/Environment.kt
+++ src/main/kotlin/no/nav/paop/Environment.kt
@@ -2,9 +2,17 @@ package no.nav.paop
 
 import java.util.Properties
 
+
+fun getEnvVar(name: String, default: String? = null): String =
+        System.getenv(name) ?: default ?: throw RuntimeException("Missing variable $name")
+
+private val config = Properties().apply {
+    putAll(Properties().apply {
+        load(Environment::class.java.getResourceAsStream("/application.properties"))
+    })
+}
+
 data class Environment(
-    val applicationPort: Int = config.getProperty("application.port").toInt(),
-    val applicationThreads: Int = config.getProperty("application.threads").toInt(),
     val srvPaopUsername: String = getEnvVar("SRVPAOP_USERNAME"),
     val srvPaopPassword: String = getEnvVar("SRVPAOP_PASSWORD"),
     val journalbehandlingEndpointURL: String = getEnvVar("JOARK_JOURNALBEHANDLING_WS_ENDPOINT_URL"),
@@ -22,22 +30,16 @@ data class Environment(
     val pdfGeneratorURL: String = getEnvVar("PDF_GENERATOR_URL", "http://pdf-gen/api"),
     val organisasjonV4EndpointURL: String = getEnvVar("VIRKSOMHET_ORGANISASJON_V4_ENDPOINTURL"),
     val kafkaBootstrapServersURL: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
-    val kafkaTopicOppfolginsplan: String = getEnvVar("PRIVAT_ALTINN_PAOP_TOPIC", "privat-altinn-paop-Mottatt"),
     val dokumentproduksjonV3EndpointURL: String = getEnvVar("DOKUMENTPRODUKSJON_V3_ENDPOINTURL"),
     val partnerEmottakEndpointURL: String = getEnvVar("PARTNERSERVICE_ENDPOINTURL"),
     val adresseregisteretV1EmottakEndpointURL: String = getEnvVar("EKSTERN_HELSE_ADRESSEREGISTERET_V1_ENDPOINTURL"),
     val receiptQueueName: String = getEnvVar("MOTTAK_QUEUE_UTSENDING_QUEUENAME"),
     val behandlealtinnmeldingV1EndpointURL: String = getEnvVar("EKSTERN_ALTINN_BEHANDLEALTINNMELDING_V1_ENDPOINTURL"),
     val altinnUserUsername: String = getEnvVar("ALTINNUSER_USERNAME"),
-    val altinnUserPassword: String = getEnvVar("ALTINNUSER_PASSWORD")
+    val altinnUserPassword: String = getEnvVar("ALTINNUSER_PASSWORD"),
+
+    val applicationPort: Int = config.getProperty("application.port").toInt(),
+    val applicationThreads: Int = config.getProperty("application.threads").toInt(),
+    val kafkaTopicOppfolginsplan: String = config.getProperty("kafka.appen.altinn.oppfolgingsplan.topic")
 
 )
-
-fun getEnvVar(name: String, default: String? = null): String =
-        System.getenv(name) ?: default ?: throw RuntimeException("Missing variable $name")
-
-private val config = Properties().apply {
-    putAll(Properties().apply {
-        load(Environment::class.java.getResourceAsStream("/application.properties"))
-    })
-}
