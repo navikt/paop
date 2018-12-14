dee7bc941a10bfc48d2057567d460ca2d2032147
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-14 17:25:15 +0100

    La til ktor client logging, oppgrader til ktor versjon 1.0.1

--- src/main/kotlin/no/nav/paop/client/HttpClient.kt
+++ src/main/kotlin/no/nav/paop/client/HttpClient.kt
@@ -9,6 +9,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
 
 fun createHttpClient() = HttpClient(CIO) {
     install(JsonFeature) {
@@ -17,6 +21,10 @@ fun createHttpClient() = HttpClient(CIO) {
             registerModule(JavaTimeModule())
             configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         }
+    }
+    install(Logging) {
+        logger = Logger.DEFAULT
+        level = LogLevel.HEADERS
     }
 }
 
