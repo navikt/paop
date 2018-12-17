62ce8ee3a51ffd355f3e32281e78d39b079f0a10
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-17 10:01:06 +0100

    La til X-Correlation-ID

--- src/main/kotlin/no/nav/paop/client/SakClient.kt
+++ src/main/kotlin/no/nav/paop/client/SakClient.kt
@@ -1,17 +1,19 @@
 package no.nav.paop.client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.url
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import no.nav.paop.Environment
 import no.nav.paop.log
+import java.util.UUID
 
 suspend fun HttpClient.generateSAK(env: Environment, domainObject: Any): String = post {
     contentType(ContentType.Application.Json)
+    header("X-Correlation-ID", UUID.randomUUID().toString())
     body = objectMapper.writeValueAsBytes(domainObject)
     log.info("Generate sak request: ${objectMapper.writeValueAsString(domainObject)}")
-
     url("${env.sakURL}/v1/saker")
 }
