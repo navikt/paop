e9de6df63cbcfff99898e5ca7addf615685ffc9d
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-17 14:35:56 +0100

    La til logging feature på sak client

--- src/main/kotlin/no/nav/paop/client/SakClient.kt
+++ src/main/kotlin/no/nav/paop/client/SakClient.kt
@@ -5,6 +5,10 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.config
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.url
@@ -28,6 +32,10 @@ class SakClient(private val endpointUrl: String) {
     }) {
         install(JsonFeature) {
             serializer = JacksonSerializer()
+        }
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
         }
     }
 
