2d375844f3ddb3dd8fddd4df59e2c41d459e182e
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-18 13:36:17 +0100

    La til Javatimemodul, for å greie å hente ut javadatetime

--- src/main/kotlin/no/nav/paop/client/SakClient.kt
+++ src/main/kotlin/no/nav/paop/client/SakClient.kt
@@ -1,5 +1,8 @@
 package no.nav.paop.client
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.config
@@ -28,7 +31,11 @@ class SakClient(private val endpointUrl: String, private val serviceUsername: St
         }
     }) {
         install(JsonFeature) {
-            serializer = JacksonSerializer()
+            serializer = JacksonSerializer{
+                registerKotlinModule()
+                registerModule(JavaTimeModule())
+                configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            }
         }
         install(BasicAuth) {
             username = serviceUsername
