c4a62b5f5634a3fbb322eb571f9cef5cb48ec108
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-18 13:51:03 +0100

    Fjernet WRITE_DATES_AS_TIMESTAMPS

--- src/main/kotlin/no/nav/paop/client/SakClient.kt
+++ src/main/kotlin/no/nav/paop/client/SakClient.kt
@@ -1,6 +1,5 @@
 package no.nav.paop.client
 
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
@@ -34,7 +33,6 @@ class SakClient(private val endpointUrl: String, private val serviceUsername: St
             serializer = JacksonSerializer {
                 registerKotlinModule()
                 registerModule(JavaTimeModule())
-                configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             }
         }
         install(BasicAuth) {
